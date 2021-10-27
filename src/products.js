bc9b47427b8b5c68af7020ac6c0b62b6d55ef0fd
Raj Kiran <88285961+rajkiran80@users.noreply.github.com>
2021-10-27 22:53:41 +0530

    Create products.js

---
+++ src/products.js
@@ -0,0 +1,47 @@
+const products=[
+    {
+       id:1,
+       name:"Hrx shirts",
+       price:99
+     },
+     {
+        id:2,
+        name:"Roadster shirts",
+        price:100
+      },{
+        id:3,
+        name:"Highlander shirts",
+        price:101
+      },
+      {
+        id:4,
+        name:"Moda Rapido shirts",
+        price:300
+      },
+      {
+        id:5,
+        name:"Travile shirts",
+        price:259
+      },
+      {
+        id:6,
+        name:"Hrx shirts",
+        price:399
+      },
+      {
+        id:7,
+        name:"Fcuk Tshirt",
+        price:450
+      },
+      {
+        id:8,
+        name:"Wrangler Jeans",
+        price:470
+      },
+      {
+        id:9,
+        name:"Jenkins",
+        price:499
+      }
+]
+export default products;
