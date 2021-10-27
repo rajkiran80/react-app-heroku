b768a04e21ccdd0881e589d026f4986947661ae0
Raj Kiran <88285961+rajkiran80@users.noreply.github.com>
2021-10-27 22:24:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,70 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const map = new Map();
+const filterPriceList=[{start:100,end:200},{start:201,end:350},{start:351,end:450},{start:451,end:500}]
+
+function Board(){
+    const [showFilteredItem,setShowFilteredItem]=useState([])
+    const showItems=showFilteredItem.length === 0 ? products : showFilteredItem
+    
+    const filterPriceFxn=(start,end,check)=>{
+         const filteredArray=[...showFilteredItem]
+         products.filter((product) => {
+             if(!map.has(product.id) && (start <= product.price) && (product.price < end) && check.checked === true){
+                 filteredArray.push(product)
+                 map.set(product.id,1)
+                 setShowFilteredItem(filteredArray)
+             }
+         })
+         if(check.checked === false){
+            let indexes=[]
+            products.map((prod,index) => {
+                if((start <= prod.price) && (prod.price <= end)){
+                    indexes.push(index)
+                    map.delete(prod.id)
+                }
+            })
+            let i=indexes.length-1
+            indexes.forEach(()=>{
+                filteredArray.splice(i,1)
+                setShowFilteredItem(filteredArray)
+                i--
+            })
+            indexes=[]
+         }
+    }
+
+    return (
+       <div className="container">
+           <div className="items">
+                {
+                  showItems.map((product) => {
+                      return (
+                                <div className="card" key={product.id}>
+                                   <div className="name">{product.name}</div>
+                                   <div className="price">{product.price}</div>
+                                </div>
+                             )
+                  })
+                }
+           </div>
+           <div className="filters card">
+                <h5>Price </h5>
+                {
+                    filterPriceList.map((filterPirce,index) => {
+                         return <label key={index}>
+                                     <input type="checkbox" onClick={(e)=>filterPriceFxn(filterPirce.start,filterPirce.end,e.target)}/> 
+                                     Rs.{filterPirce.start} to Rs.{filterPirce.end}
+                                </label>
+                   })
+                }
+           </div>
+       </div>
+    )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Board />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
